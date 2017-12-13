cf5e834dcff2f58638a1ef2e78839fcdf3033deb
lzg61818678 <lzg47@vip.qq.com>
2017-12-13 15:39:02 +0800

    delete

--- src/com/shop/dao/AdminDao.java
+++
@@ -1,7 +0,0 @@
-package com.shop.dao;
-
-import com.shop.model.Admin;
-
-public interface AdminDao extends BaseDao<Admin>{
-	public Admin findByAdminnameAndPassword(String username,String password);
-}
